24b98d7468b4ba4612aac8ce927c6ce9f99f39df
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2021-11-05 17:59:38 +0000

    first java edit

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,12 +7,15 @@ class Dec2Hex
 
     public static void main(String args[])
     {
+        if (args.length == 0){
+            System.out.println("You cannot run this code without a command line argument");
+        } else {
         Arg1 = Integer.parseInt(args[0]);
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
         String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex....");
+        System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
         {
@@ -23,6 +26,7 @@ class Dec2Hex
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
 
+    }
     }
 }
 
