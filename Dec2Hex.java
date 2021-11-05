919b7837ba471f8ecb281c389e8781011fec7249
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2021-11-05 18:27:55 +0000

    second java edit

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -10,7 +10,12 @@ class Dec2Hex
         if (args.length == 0){
             System.out.println("You cannot run this code without a command line argument");
         } else {
+            try{
         Arg1 = Integer.parseInt(args[0]);
+        }
+        catch(Exception e) {
+            System.out.println("You must enter a whole number");
+        }
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
