50182c3a554778503f5fa112b1b6c551fb0905d1
awkwardwaltz <OMURPH202@caledonian.ac.uk>
2021-11-08 13:01:58 +0000

    Changes for testing

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,25 +1,30 @@
+package com.devops.com.devops.maven.eclipse;
 import java.util.Scanner;
 
 class Dec2Hex
 {
 
     public static int Arg1;
-
+    public static boolean noArgument;
+    public static String hexadecimal;
     public static void main(String args[])
     {
         if (args.length == 0){
+        	noArgument = true;
             System.out.println("You cannot run this code without a command line argument");
         } else {
             try{
+            noArgument = false;
         Arg1 = Integer.parseInt(args[0]);
         }
         catch(Exception e) {
-            System.out.println("You must enter a whole number");
+            System.out.println("You must enter a whole number.");
         }
         char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
         int rem, num;
         num = Arg1;
-        String hexadecimal="";
+        hexadecimal="";
+        if (num != 0){
         System.out.println("Converting the Decimal Value " + num + " to Hex...");
 
         while(num != 0)
@@ -30,7 +35,7 @@ class Dec2Hex
         }
 
         System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+    } 
     }
     }
 }
